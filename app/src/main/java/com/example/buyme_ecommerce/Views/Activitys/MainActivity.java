0ad4b148cb8ca4ad1a2f180005b0423efcbdbedb
CassanoQuan <nguyenanhquan01102002@gmail.com>
2023-04-08 08:55:07 +0700

    buyme

---
+++ app/src/main/java/com/example/buyme_ecommerce/Views/Activitys/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.buyme_ecommerce.Views.Activitys;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.buyme_ecommerce.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
